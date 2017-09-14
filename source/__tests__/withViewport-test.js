516be89b88451ceaaa77c6be1f9e0abf89b28fd8
petehurst <petehurst@users.noreply.github.com>
2017-09-14 23:07:47 +0100

    More tests, last one failing (see commit notes)
    Due to the component using a global variable to handle SSR, tests start
    failing as soon as a 2nd test runs. Might "fix" this temporarily by
    reloading the module between tests via require-reload,  as this is not
    real-world behavior yet.

--- source/__tests__/withViewport-test.js
+++ source/__tests__/withViewport-test.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import withViewport, {
     DEFAULT_BROWSERLESS_WIDTH,
     DEFAULT_BROWSERLESS_HEIGHT
@@ -70,4 +70,50 @@ test("Cannot override width and height if not rehydrating", () => {
     expect(entity.find(MockEntity).node.props.viewportHeight).toBe(
         JSDOM_DEFAULT_HEIGHT
     );
+});
+
+test("In rehydrate mode, width and height eventually take window values", done => {
+    const Wrapped = withViewport({ handleRehydration: true })(MockEntity);
+    const entity = mount(<Wrapped />);
+    expect(entity.find(MockEntity).node.props.viewportWidth).toBe(
+        DEFAULT_BROWSERLESS_WIDTH
+    );
+    expect(entity.find(MockEntity).node.props.viewportHeight).toBe(
+        DEFAULT_BROWSERLESS_HEIGHT
+    );
+    setImmediate(() => {
+        expect(entity.find(MockEntity).node.props.viewportWidth).toBe(
+            JSDOM_DEFAULT_WIDTH
+        );
+        expect(entity.find(MockEntity).node.props.viewportHeight).toBe(
+            JSDOM_DEFAULT_HEIGHT
+        );
+        done();
+    });
+});
+
+test("In rehydrate mode, width and height eventually take overidden values", done => {
+    const otherWidth = 5678;
+    const otherHeight = 1234;
+    const Wrapped = withViewport({
+        browserlessWidth: otherWidth,
+        browserlessHeight: otherHeight,
+        handleRehydration: false
+    })(MockEntity);
+    const entity = mount(<Wrapped />);
+    expect(entity.find(MockEntity).node.props.viewportWidth).toBe(
+        DEFAULT_BROWSERLESS_WIDTH
+    );
+    expect(entity.find(MockEntity).node.props.viewportHeight).toBe(
+        DEFAULT_BROWSERLESS_HEIGHT
+    );
+    setImmediate(() => {
+        expect(entity.find(MockEntity).node.props.viewportWidth).toBe(
+            otherWidth
+        );
+        expect(entity.find(MockEntity).node.props.viewportHeight).toBe(
+            otherHeight
+        );
+        done();
+    });
 });
