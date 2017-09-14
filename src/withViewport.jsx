d4b9cb74d9b74216d5fc9e020c12d08afdb71e24
petehurst <petehurst@users.noreply.github.com>
2017-09-14 21:07:22 +0100

    Comment code a little bit

--- src/withViewport.jsx
+++ src/withViewport.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import hoistStatics from "hoist-non-react-statics";
 
+// Define some constants
 const DEFAULT_BROWSERLESS_WIDTH = 1920;
 const DEFAULT_BROWSERLESS_HEIGHT = 1080;
 
 const DOM_RESIZE_EVENT = "resize";
 
+// Gets either the real or fake width and height
 const getState = (width, height) =>
     typeof window !== "undefined"
         ? {
@@ -18,32 +20,45 @@ const getState = (width, height) =>
               height
           };
 
+let handledRehydration = false;
+let handlingRehyrdation = false;
+
+// HOC signature
 const withViewport = ({
     browserlessWidth = DEFAULT_BROWSERLESS_WIDTH,
-    browserlessHeight = DEFAULT_BROWSERLESS_HEIGHT
+    browserlessHeight = DEFAULT_BROWSERLESS_HEIGHT,
+    handleRehydration = false
 }) => Element => {
+    // Get friendly display name to be shown in React devtools
     const displayName = Element.displayName || Element.name || "Component";
 
+    // Generate the wrapped component class
     const Wrapped = class WithViewport extends Component {
+        // For React devtools
         static displayName = `WithViewport(${displayName})`;
         static WrappedComponent = Element;
 
+        // Initialize state
         state = getState();
 
+        // Bind resize listener
         componentDidMount() {
             window.addEventListener(DOM_RESIZE_EVENT, this.handleResize);
         }
 
+        // Unbind resize listener when component unmounts
         componentWillUnmount() {
             window.removeEventListener(DOM_RESIZE_EVENT, this.handleResize);
         }
 
+        // Update local state on resize
         handleResize = () => {
             this.setState(getState(browserlessWidth, browserlessHeight));
         };
 
         render() {
             const { children, ...others } = this.props;
+            // Pass props onto wrapped element
             return (
                 <Element
                     {...others}
@@ -55,6 +70,7 @@ const withViewport = ({
             );
         }
     };
+    // Lift any static methods
     hoistStatics(Wrapped, Element);
     return Wrapped;
 };
