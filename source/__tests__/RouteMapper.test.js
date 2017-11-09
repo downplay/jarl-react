f776a445652d8fdaf4153a5ee4821c8c3beef110
petehurst <petehurst@users.noreply.github.com>
2017-11-09 21:12:15 +0000

    Sanity check test

---
+++ source/__tests__/RouteMapper.test.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { shallow, mount } from "enzyme";
+import RouteMapper from "../RouteMapper";
+
+class MockEntity extends Component {
+    render() {
+        return null;
+    }
+}
+
+test("it constructs", () => {
+    expect(new RouteMapper()).toBeInstanceOf(RouterMapper);
+});
