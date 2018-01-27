352343fccac6e4710fccd60c1cfe1eb8a7cf91f2
petehurst <petehurst@users.noreply.github.com>
2018-01-27 19:35:14 +0000

    Strip crust from SimpleProvider also

--- packages/jarl-react/source/SimpleProvider.js
+++ packages/jarl-react/source/SimpleProvider.js
@@ -1,41 +1,14 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 
 import NavigationProvider from "./NavigationProvider";
-import RouteMapper from "./RouteMapper";
 
 export default class SimpleProvider extends Component {
-    static propTypes = {
-        routes: PropTypes.oneOfType([
-            PropTypes.array,
-            PropTypes.instanceOf(RouteMapper)
-        ]).isRequired,
-        history: PropTypes.object.isRequired
-    };
-
     /**
      * Initalise route mapper and setup state from current URL
      */
     constructor(props) {
         super(props);
         this.state = { navigationState: {} };
-        this.ensureRouteMapper(props.routes);
-        const path = props.history.location.pathname;
-        const match = this.routeMapper.match(path);
-        if (match) {
-            // Mutation here is fine while setting up initial state
-            this.state.navigationState = match.state;
-        }
-    }
-
-    /**
-     * If different routes passed in for any reason, make sure
-     * we use the right RouteMapper
-     */
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.routes !== this.props.routes) {
-            this.ensureRouteMapper(nextProps.routes);
-        }
     }
 
     /**
@@ -45,17 +18,12 @@ export default class SimpleProvider extends Component {
         this.setState({ navigationState: state });
     };
 
-    ensureRouteMapper = routes => {
-        this.routeMapper =
-            routes instanceof RouteMapper ? routes : new RouteMapper(routes);
-    };
-
     render() {
-        const { children, history } = this.props;
+        const { children, routes, ...others } = this.props;
 
         return (
             <NavigationProvider
-                history={history}
+                {...others}
                 routes={this.routeMapper}
                 state={this.state.navigationState}
                 onNavigateEnd={this.handleNavigateEnd}
