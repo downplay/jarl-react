623960d1ca8fe328efb55ff021c40f8bd1a4cf50
Peter Hurst <peter.hurst@90poe.io>
2018-02-22 12:39:54 +0000

    Use exports more safely

--- packages/jarl-react/tool/safeJsonStringify.js
+++ packages/jarl-react/tool/safeJsonStringify.js
@@ -1,6 +1,8 @@
 "use strict";
 
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
 var safeJsonStringify = function safeJsonStringify(o) {
     try {
         return JSON.stringify(o);
