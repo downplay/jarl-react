90c7c712b8537cb3ddd895126dc5d7a8e07bb17b
Peter Hurst <peter.hurst@90poe.io>
2018-04-07 21:07:09 +0100

    Give server a process title

--- demo/server/ci.js
+++ demo/server/ci.js
@@ -1,6 +1,10 @@
 import path from "path";
 import reactServer from "./reactServer";
 
+// In CI our server process needs to have a title so we can
+// kill it from the command line after running E2E
+process.title = "JARLDEMO";
+
 const context = {
     basePath: path.resolve(__dirname, ".."),
     name: "JARL demos",
